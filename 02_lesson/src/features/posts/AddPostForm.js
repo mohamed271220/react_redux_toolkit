a594fb184c5e9347fa0388954d883733980a9e15
mohamed271220 <mohamedaprahym@gmail.com>
2023-04-14 01:47:02 +0200

    comments on lesson 2

--- 02_lesson/src/features/posts/AddPostForm.js
+++ 02_lesson/src/features/posts/AddPostForm.js
@@ -1,72 +1,70 @@
 //we use useState now for temporary states
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-
+import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
-    const dispatch = useDispatch()
+  /** It allows you to dispatch actions to the Redux store from within a functional component by using the useDispatch hook from the react-redux library. */
+  const dispatch = useDispatch();
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const [userId, setUserId] = useState('')
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [userId, setUserId] = useState("");
+  //seSelector is a hook from React-Redux that allows you to extract data from the Redux store state using a selector function.
+  const users = useSelector(selectAllUsers);
 
-    const users = useSelector(selectAllUsers)
+  const onTitleChanged = (e) => setTitle(e.target.value);
+  const onContentChanged = (e) => setContent(e.target.value);
+  const onAuthorChanged = (e) => setUserId(e.target.value);
 
-    const onTitleChanged = e => setTitle(e.target.value)
-    const onContentChanged = e => setContent(e.target.value)
-    const onAuthorChanged = e => setUserId(e.target.value)
-
-    const onSavePostClicked = () => {
-        if (title && content) {
-            dispatch(
-                postAdded(title, content, userId)
-            )
-            setTitle('')
-            setContent('')
-        }
+  const onSavePostClicked = () => {
+    if (title && content) {
+      dispatch(postAdded(title, content, userId));
+      setTitle("");
+      setContent("");
     }
+  };
 
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+  // if all of the are true and upon that we will disable of enable the save button
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId);
 
-    const usersOptions = users.map(user => (
-        <option key={user.id} value={user.id}>
-            {user.name}
-        </option>
-    ))
+  const usersOptions = users.map((user) => (
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ));
 
-    return (
-        <section>
-            <h2>Add a New Post</h2>
-            <form>
-                <label htmlFor="postTitle">Post Title:</label>
-                <input
-                    type="text"
-                    id="postTitle"
-                    name="postTitle"
-                    value={title}
-                    onChange={onTitleChanged}
-                />
-                <label htmlFor="postAuthor">Author:</label>
-                <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
-                    <option value=""></option>
-                    {usersOptions}
-                </select>
-                <label htmlFor="postContent">Content:</label>
-                <textarea
-                    id="postContent"
-                    name="postContent"
-                    value={content}
-                    onChange={onContentChanged}
-                />
-                <button
-                    type="button"
-                    onClick={onSavePostClicked}
-                    disabled={!canSave}
-                >Save Post</button>
-            </form>
-        </section>
-    )
-}
-export default AddPostForm
+  return (
+    <section>
+      <h2>Add a New Post</h2>
+      <form>
+        <label htmlFor="postTitle">Post Title:</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={title}
+          onChange={onTitleChanged}
+        />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
+        <label htmlFor="postContent">Content:</label>
+        <textarea
+          id="postContent"
+          name="postContent"
+          value={content}
+          onChange={onContentChanged}
+        />
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
+          Save Post
+        </button>
+      </form>
+    </section>
+  );
+};
+export default AddPostForm;
